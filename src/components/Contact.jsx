9278615fd611eb9f1bc06f3d827f72e79d951aa8
beastrobel <beatrizstrobel@proton.me>
2024-01-07 03:33:54 -0500

    Final commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,80 @@
+import { useState } from 'react';
+
+// Here we import a helper function that will check if the email is valid
+import { validateEmail } from '../utils/helpers';
+
 function Contact() {
-    return(
-        <div class="container">
-        <div class = "row">
-            <h2>Contact</h2>
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setName('');
+    setMessage('');
+    setEmail('');
+  };
+
+  return (
+    <section>
+    <h2>Contact</h2>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <p>Name</p>
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="name"
+        /><br/><br/>
+        <p>Email</p>
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        /><br/><br/>
+        <p>Message</p>
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="message"
+          id="message"
+        /><br/><br/>
+        <button class="btn btn-primary" type="submit">Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
         </div>
-        </div>   
-    );
+      )}
+    </section>
+  );
 }
-    
-export default Contact; 
+
+export default Contact;

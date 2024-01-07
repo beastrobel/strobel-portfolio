dc8968df29edae699cd88a55d2b00c2c9942d737
beastrobel <beatrizstrobel@proton.me>
2024-01-07 02:36:55 -0500

    About section complete

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,17 @@ function About() {
         <section>
         <div class="row">
         <div class="col-sm-12 col-md-12 col-lg-12 m-3 p-5" id="name-and-title">
+        <img src="src/assets/BeatrizStrobel.jpg" id="aboutimg"/>
         <h2>Hi, I'm Beatriz Strobel.</h2>
         <h1>I develop user-friendly interfaces.</h1>
-        <p>Front-end web developer based in Grand Rapids, MI.</p>
+        <p>Web developer based in Grand Rapids, MI.</p>
+        <br/>
+        <br/>
+        <p>
+           I am a jack of all trades. With just under 10 years of experience as a Graphic Designer and UI/UX Designer, I am a styling expert. 
+           I also bring to the table full-stack web development skills in React.js, Apollo GraphQL, Express.js, MongoDB, MySQL, Node.js,
+          HTML, CSS, Bootstrap, JavaScript and jQuery. I look forward to working with you!  
+        </p>
         </div>
       </div>  
       </section>

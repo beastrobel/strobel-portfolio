fd85d58ba964eeb2c49d3655f27e6b196ec4a056
beastrobel <beatrizstrobel@proton.me>
2024-01-07 03:48:48 -0500

    Adjusted image paths for Netlify

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,37 +3,37 @@ function Portfolio() {
         <section>
         <div class="container">
         <div class = "row">
-          <img src="/src/assets/TechBlog.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
+          <img src="/TechBlog.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
           <h3 class="col-sm-12 col-lg-12">Tech Blog</h3><br/><br/>
           <p class="col-sm-12 col-lg-12"><a href="https://tech-blog-42-62e93e33c17a.herokuapp.com/" target="blank">Deployed web application</a></p>
           <p class="col-sm-12 col-lg-12"><a href="https://github.com/beastrobel/tech-blog" target="blank">GitHub repository</a></p> 
           </div> 
         <div class = "row">
-        <img src="/src/assets/HotspotFinderMockup.jpg" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
+        <img src="/HotspotFinderMockup.jpg" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
         <h3 class="col-sm-12 col-lg-12">Hotspot Finder</h3><br/><br/>
         <p class="col-sm-12 col-lg-12"><a href="https://beastrobel.github.io/Hotspot-Finder-2.0" target="blank">Deployed web application</a></p>  
         <p class="col-sm-12 col-lg-12"><a href="https://github.com/beastrobel/Hotspot-Finder-2.0" target="blank">GitHub repository</a></p>  
         </div>
         <div class = "row">
-          <img src="/src/assets/UmbrellaWeather.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
+          <img src="/UmbrellaWeather.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
           <h3 class="col-sm-12 col-lg-12">Umbrella Weather</h3><br/><br/>
           <p class="col-sm-12 col-lg-12"><a href="https://beastrobel.github.io/umbrella" target="blank">Deployed web application</a></p>
           <p class="col-sm-12 col-lg-12"><a href="https://github.com/beastrobel/umbrella" target="blank">GitHub repository</a></p> 
           </div>
           <div class = "row">
-          <img src="/src/assets/CodeCanvas.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
+          <img src="/CodeCanvas.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
           <h3 class="col-sm-12 col-lg-12">Code Canvas</h3><br/><br/>
           <p class="col-sm-12 col-lg-12"><a href="https://code-canvas-11d87b39b641.herokuapp.com" target="blank">Deployed web application</a></p>
           <p class="col-sm-12 col-lg-12"><a href="https://github.com/LanceLancealot/CodeCanvas" target="blank">GitHub repository</a></p> 
           </div>
           <div class = "row">
-          <img src="/src/assets/SocialMediaAPI.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
+          <img src="/SocialMediaAPI.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
           <h3 class="col-sm-12 col-lg-12">Social Network API</h3><br/><br/>
           <p class="col-sm-12 col-lg-12"><a href="https://drive.google.com/file/d/1aiwursJiMkUHEBV5Qzg49Hhs4-5IbR2P/view?usp=drive_link" target="blank">Video showing functionality</a></p>
           <p class="col-sm-12 col-lg-12"><a href="https://github.com/beastrobel/social-network-api" target="blank">GitHub repository</a></p> 
           </div>
           <div class = "row">
-          <img src="/src/assets/ECommerce.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
+          <img src="/ECommerce.png" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
           <h3 class="col-sm-12 col-lg-12">E-Commerce Back End</h3><br/><br/>
           <p class="col-sm-12 col-lg-12"><a href="https://drive.google.com/file/d/1AjcfORVPXFaEOvE0rvRrdVKzZtBmY0YN/view?usp=drive_link" target="blank">Video showing functionality</a></p>
           <p class="col-sm-12 col-lg-12"><a href="https://github.com/beastrobel/e-commerce" target="blank">GitHub repository</a></p> 

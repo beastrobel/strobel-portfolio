10c3b7be17da5a73ea9079961761802df98ba3c2
beastrobel <beatrizstrobel@proton.me>
2024-01-07 03:43:32 -0500

    Adjusted image paths for Netlify

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ function About() {
         <section>
         <div class="row">
         <div class="col-sm-12 col-md-12 col-lg-12 m-3 p-5" id="name-and-title">
-        <img src="src/assets/BeatrizStrobel.jpg" id="aboutimg"/>
+        <img src="/src/assets/BeatrizStrobel.jpg" id="aboutimg"/>
         <h2>Hi, I'm Beatriz Strobel.</h2>
         <h1>I develop user-friendly interfaces.</h1>
         <p>Web developer based in Grand Rapids, MI.</p>
@@ -14,7 +14,7 @@ function About() {
            I also bring to the table full-stack web development skills in React.js, Apollo GraphQL, Express.js, MongoDB, MySQL, Node.js,
           HTML, CSS, Bootstrap, JavaScript and jQuery. I look forward to working with you!  
         </p>
-        <img src="./src/assets/HotspotFinderMockup.jpg" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
+        <img src="/src/assets/HotspotFinderMockup.jpg" class="mx-auto d-block col-12 col-md-12 col-lg-12 p-5"/>
         </div>
       </div>  
       </section>
